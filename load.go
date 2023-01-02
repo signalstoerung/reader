d63d08c5c11817b5dd4ab1e4ded6dce5e30e574d
Niclas <nm@xistenz.com>
2023-01-02 19:58:05 +0100

    pagination works

--- load.go
+++ load.go
@@ -5,14 +5,14 @@ import (
 	"gorm.io/gorm"
 )
 
-func loadItemsFromDB(db *gorm.DB, limit int, offset int) error {
+func loadItemsFromDB(db *gorm.DB, resultSlice *[]string, limit int, offset int) error {
 	var items []Item
 	result := db.Limit(limit).Offset(offset).Order("published_parsed desc").Find(&items)
 	if result.Error != nil {
 		return result.Error
 	}
 	for _,item := range items {
-		fmt.Printf("<div><a href=\"%v\">%v -- %v</a></div>\n",item.Link,item.PublishedParsed.Format("02 Jan 15:04"),item.Title)
+		*resultSlice = append(*resultSlice, fmt.Sprintf("<div><a href=\"%v\">%v -- %v</a></div>\n",item.Link,item.PublishedParsed.Format("02 Jan 15:04"),item.Title))
 	}
 	return nil
 }
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"gorm.io/gorm"
 	"net/http"
 	"time"
+	"os"
+	"strconv"
 )
 
 type Feed struct {
@@ -22,6 +24,14 @@ type Item struct {
 	PublishedParsed *time.Time
 }
 
+var db *gorm.DB
+
+func openDBConnection()  error {
+	var err error
+	db, err = gorm.Open(sqlite.Open("reader.db"), &gorm.Config{})
+	return err
+}
+
 func initializeDB (db *gorm.DB) {
 	db.AutoMigrate(&Feed{})
 	db.AutoMigrate(&Item{})
@@ -30,30 +40,56 @@ func initializeDB (db *gorm.DB) {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, web.")
-	fmt.Println(r)
+	limit := 15
+	page := 0
+	offset := 0
+	result := make([]string,0,limit)
+	
+	pageQuery := r.URL.Query()
+	if pageQuery.Get("page") != "" {
+		p, err := strconv.Atoi(pageQuery.Get("page"))
+		if err == nil {
+			page = p
+			offset = p*limit
+		}
+	}
+	err := loadItemsFromDB(db, &result, limit, offset)
+	if err != nil {
+		fmt.Printf("Error in rootHandler: ",err)
+	}
+	for _,s := range result {
+		fmt.Fprintf(w, s)
+	}
+	if page > 0 {
+		fmt.Fprintf(w, "<a href=\"/?page=%v\">previous</a>",page-1)
+	}
+		fmt.Fprintf(w, "| Page %v | <a href=\"/?page=%v\">next</a>",page,page+1)
 }
 
+
 func main() {
+	if err := openDBConnection(); err != nil {
+		fmt.Printf("encountered an error: %v", err)
+		os.Exit(1)
+	}
+
+// 	initializeDB(db)	
+
+
+	http.HandleFunc("/", rootHandler)
+
 	// 	// handler for dynamic content
-	// 	http.HandleFunc("/", rootHandler)
 	//
 	// 	// handler for static files
 	// 	staticFileHandler := http.FileServer(http.Dir("./www"))
 	// 	http.Handle("/static/", staticFileHandler)
 	//
-	// 	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", nil)
 
-	db, err := gorm.Open(sqlite.Open("reader.db"), &gorm.Config{})
-	if err != nil {
-		fmt.Printf("encountered an error: %v", err)
-	}
-	
-// 	initializeDB(db)	
 
 // 	err = ingestFromDB(db)
-	err = loadItemsFromDB(db, 15, 10)
-	if err != nil {
-		fmt.Printf("encountered an error: %v", err)
-	}
+// 	err = loadItemsFromDB(db, 15, 10)
+// 	if err != nil {
+// 		fmt.Printf("encountered an error: %v", err)
+// 	}
 }
